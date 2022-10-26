29592b23287a7007c3c35374111281998d4ab03b
Paola Guerra <paola@192.168.1.9>
2022-10-25 23:57:15 -0300

    Node

--- 01-Node/homework/commands/index.js
+++ 01-Node/homework/commands/index.js
@@ -12,6 +12,15 @@ module.exports = {
         done(arg.join(' '));
     },
     ls: function (arg,done){
-
+        fs.readdir('.', function(err,data){ // readdir devuelve un arreglo con los alementos que hay en el dir
+            if (err) throw err;
+            done(data.join('\n')); // data es el arreglo y lo junto
+        })
+    },
+    cat: function(arg,done){
+        fs.readFile(arg[0], 'utf-8', function(err, data){
+            if (err) throw err;
+            done (data);
+        })
     }
 }
