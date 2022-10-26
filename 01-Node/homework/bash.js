1463f8d081da7e92f8a2973e38e79c85611a16e8
Paola Guerra <paola@192.168.1.9>
2022-10-25 23:24:13 -0300

    Node

--- 01-Node/homework/bash.js
+++ 01-Node/homework/bash.js
@@ -1,1 +1,32 @@
-4
+ const commands = require('./commands');
+ // coomands pasa a ser un obj con funt ejecutables {date(),pwd()} 
+
+ //se crea done con la funcion de impresion para no repetir
+const done = function(data){
+    process.stdout.write(data);
+    process.stdout.write('\nprompt > ');
+}
+
+
+// El evento stdin 'data' se dispara cuando el user escribe una línea
+// process.stdin.on('data', function (data) {
+//   var cmd = data.toString().trim(); // remueve la nueva línea
+//   if(cmd === 'date') {
+//     commands.date(done);  
+//   }
+//   if(cmd === 'pwd') {
+//     commands.pwd(done);
+//   }
+// });
+// 
+// Se resume todo lo hecho en una funcion que sirva para ejecuatr cualquier comando pasado por parametro.
+
+process.stdin.on('data', function (data) {
+    let arg = data.toString().trim().split(' ');
+    let cmd = arg.shift(); 
+    if(commands[cmd]) {     // verifico si lo pasado es una propiedad existente
+        commands[cmd](arg,done);       // ejecuto la funcion de esa propiedad
+    } else {
+        done('Comando no existente');   //muestro el sig cartel
+    }
+  });
--- 01-Node/homework/commands/index.js
+++ 01-Node/homework/commands/index.js
@@ -1,5 +1,17 @@
+var fs = require('fs'); // es una biblioteca nativa de Node
 
 
 module.exports = {
+    date: function(arg,done){
+        done(Date());
+    },
+    pwd: function(arg,done){
+        done(process.cwd());
+    },
+    echo: function(arg,done){
+        done(arg.join(' '));
+    },
+    ls: function (arg,done){
 
+    }
 }
